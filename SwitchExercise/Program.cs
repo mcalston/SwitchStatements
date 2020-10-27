865dac202f28a2ccf6ea58cc7052927f6d859c27
Miles Alston <milescalston@gmail.com>
2020-10-27 15:15:00 -0400

    Add project files.

---
+++ SwitchExercise/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SwitchStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"What's your favorite subject?");
+            var response = Console.ReadLine();
+
+            switch (response)
+            {
+                case "Art":
+                    Console.WriteLine($"Art is fun!");
+                    break;
+                case "English":
+                    Console.WriteLine("That's cool but I cannot read");
+                    break;
+                case "Science":
+                    Console.WriteLine("A bit complicated, but important to know");
+                    break;
+                case "Math":
+                    Console.WriteLine("I HATE math with a passion");
+                    break;
+                case "Gym":
+                    Console.WriteLine("Gotta get that exercise going!");
+                    break;
+                default:
+                    Console.WriteLine("Never heard of that one");
+                    break;
+            }
+        }
+    }
+}
